38d69f32491b6f0e7921341c769d18ce6dc431c7
aissi_ayoub <picrox@picrox.lan>
2020-01-28 14:49:07 +0100

    Configuratio automatique de SSH et des comptes utilisateurs

--- VM/provision.sh
+++ VM/provision.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Script : provion.sh
+#Author : Aissi Ayoub
+#Last edit : 28 janv 2020 || 02 : 16 
 
 VM=$1
 if [ "$VM" = "rundeck" ];then
@@ -12,9 +15,23 @@ if [ "$VM" = "rundeck" ];then
     sudo firewall-cmd --zone=public --add-port=4440/tcp --permanent
     sudo firewall-cmd --reload
     sudo systemctl restart rundeckd
+    sudo -u root adduser adminRundeck
+    sudo -u root echo rundeck | sudo -u root passwd adminRundeck --stdin 
+    sudo -u root gpasswd -a adminRundeck wheel
+    sudo -u adminRundeck ssh-keygen -t rsa -N "" -f /home/adminRundeck/.ssh/id_rsa
+    sudo yum -y install wget
+    sudo wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    sudo rpm -ivh epel-release-6-8.noarch.rpm
+    sudo yum --enablerepo=epel -y install sshpass
+    sudo -u adminRundeck ssh -o "StrictHostKeyChecking no" rundeck@172.28.128.4
+    sudo -u adminRundeck sshpass -p rundeck ssh-copy-id rundeck@172.28.128.4
+    sudo -u adminRundeck ssh -o "StrictHostKeyChecking no" rundeck@172.28.128.3
+    sudo -u adminRundeck sshpass -p rundeck ssh-copy-id rundeck@172.28.128.3
+    sudo -u adminRundeck ssh -o "StrictHostKeyChecking no" rundeck@172.28.128.2
+    sudo -u adminRundeck sshpass -p rundeck ssh-copy-id rundeck@172.28.128.2
 fi
-
-if [ "$VM" = "mail" ];then
+if [ "$VM" = "mail" ]
+then
     sudo apt-get -y update
     sudo apt-get -y install vim
     sudo apt-get -y install nano
@@ -22,7 +39,16 @@ if [ "$VM" = "mail" ];then
     sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
     sudo apt-get -y install postfix sasl2-bin
     sudo apt-get -y install nfs-common
-
+    sudo -u root adduser --quiet --disabled-password --shell /bin/bash --home /home/rundeck --gecos "User" rundeck
+    sudo -u root echo "rundeck:rundeck" | sudo -u root chpasswd
+    sudo -u root usermod -aG sudo rundeck
+    sudo -u rundeck ssh-keygen -t rsa -N "" -f /home/rundeck/.ssh/id_rsa
+    sudo -u rundeck echo rundeck && sudo -S chmod 777 /etc/ssh/sshd_config
+    sudo -u rundeck echo rundeck && sudo -S sed -i '$d' /etc/ssh/sshd_config 
+    sudo -u rundeck echo rundeck && sudo -S echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+    sudo -u rundeck echo rundeck && sudo -S echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+    sudo -u rundeck echo rundeck && sudo -S chmod 644 /etc/ssh/sshd_config 
+    sudo -u rundeck echo rundeck && sudo -S systemctl restart sshd
 fi
 
 if [ "$VM" = "web" ];then
@@ -30,11 +56,29 @@ if [ "$VM" = "web" ];then
     sudo apt-get -y install vim
     sudo apt-get -y install apache2
     sudo apt-get -y install nfs-common
+    sudo -u root adduser --quiet --disabled-password --shell /bin/bash --home /home/rundeck --gecos "User" rundeck
+    sudo -u root echo "rundeck:rundeck" | sudo -u root chpasswd
+    sudo -u root usermod -aG sudo rundeck
+    sudo -u rundeck ssh-keygen -t rsa -N "" -f /home/rundeck/.ssh/id_rsa
+    sudo -u rundeck echo rundeck && sudo -S chmod 777 /etc/ssh/sshd_config
+    sudo -u rundeck echo rundeck && sudo -S sed -i '$d' /etc/ssh/sshd_config 
+    sudo -u rundeck echo rundeck && sudo -S echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+    sudo -u rundeck echo rundeck && sudo -S echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+    sudo -u rundeck echo rundeck && sudo -S chmod 644 /etc/ssh/sshd_config 
+    sudo -u rundeck echo rundeck && sudo -S systemctl restart sshd
 fi
 if [ "$VM" = "nfs" ];then
     sudo apt-get -y update
     sudo apt-get -y install vim
     sudo apt-get -y install nfs-common
+    sudo -u root adduser --quiet --disabled-password --shell /bin/bash --home /home/rundeck --gecos "User" rundeck
+    sudo -u root echo "rundeck:rundeck" | sudo -u root chpasswd
+    sudo -u root usermod -aG sudo rundeck
+    sudo -u rundeck ssh-keygen -t rsa -N "" -f /home/rundeck/.ssh/id_rsa
+    sudo -u rundeck echo rundeck && sudo -S chmod 777 /etc/ssh/sshd_config
+    sudo -u rundeck echo rundeck && sudo -S sed -i '$d' /etc/ssh/sshd_config 
+    sudo -u rundeck echo rundeck && sudo -S echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+    sudo -u rundeck echo rundeck && sudo -S echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+    sudo -u rundeck echo rundeck && sudo -S chmod 644 /etc/ssh/sshd_config 
+    sudo -u rundeck echo rundeck && sudo -S systemctl restart sshd
 fi
-
-# vim: et sw=4
